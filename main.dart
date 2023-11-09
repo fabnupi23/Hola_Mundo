f966a34e3888558b0f1f978cadc8e50fc99d872c
Fabian_Nuñez <31360808+fabnupi23@users.noreply.github.com>
2023-11-09 11:55:48 -0500

    Hola Mundo dart

---
+++ main.dart
@@ -0,0 +1,10 @@
+//En esre archivo vamos a definir la función que representa la ejecución de nuestro proyecto Dark eso tambien incluye a flutter cuando
+//entremos a desarrollar aplicaciones moviles 
+
+//La función main que es la que vamos a crear tiene la siguiente estructura
+
+//El tipo de dato que retorna la función
+
+void main(){
+  print("Hello, world!");
+}
