a119aaa8b3c30d23fcac2eb5b2606f537073fbd0
Fabian_Nuñez <31360808+fabnupi23@users.noreply.github.com>
2023-11-09 12:01:33 -0500

    Notas

--- main.dart
+++ main.dart
@@ -1,10 +1,13 @@
 //En esre archivo vamos a definir la función que representa la ejecución de nuestro proyecto Dark eso tambien incluye a flutter cuando
-//entremos a desarrollar aplicaciones moviles 
+//entremos a desarrollar aplicaciones moviles
 
 //La función main que es la que vamos a crear tiene la siguiente estructura
 
 //El tipo de dato que retorna la función
 
-void main(){
+void main() {
   print("Hello, world!");
 }
+
+//Un proyecto dart debe contener la función main para inciar con la ejecución del proyecto y luego para correr nuestro proyecto basta con utilizar
+//dart run y luego el nombre del archivo que contiene la función main
